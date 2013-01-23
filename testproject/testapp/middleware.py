98ebebe940ca4043dffe4c5585c387f73db4d21c
animasola <annima.sola@gmail.com>
2013-01-23 21:41:52 +0200

    ticket:3 Create new model for saved requests

---
+++ testproject/testapp/middleware.py
@@ -0,0 +1,12 @@
+from testproject.testapp.models import RequestsLog
+
+
+class RequestLogger(object):
+    
+    def process_request(self, request):
+        if request:
+            req = RequestsLog(requested_url\
+                =request.build_absolute_uri(request.path),
+            request_type=request.method,
+            request_ip=request.META['REMOTE_ADDR'])
+        req.save()
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -2,10 +2,16 @@
 from annoying.decorators import render_to
 from annoying.functions import get_object_or_None
 
-from models import PersonalInfo
+from models import PersonalInfo, RequestsLog
 
 
 @render_to('mainpage.html')
 def show_personal_info(request):
     my_info = get_object_or_None(PersonalInfo, pk=1)
     return {'my_info': my_info}
+
+@render_to('requests_log.html')
+def requests_log_page(request):
+    first_ten_requests =\
+        RequestsLog.objects.filter().order_by('request_timestamp')[: 10]
+    return {'requests': first_ten_requests}
