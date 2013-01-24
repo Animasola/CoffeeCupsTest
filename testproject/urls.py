12ee597ff4ad4818834cbf9ec28253ad9ec1fcd9
animasola <annima.sola@gmail.com>
2013-01-24 20:10:46 +0200

    ticket:3 enabled admin interface

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,23 +1,14 @@
 from django.conf.urls import patterns, include, url
 from testapp.views import show_personal_info, requests_log_page
 from django.conf import settings
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', show_personal_info, name='mainpage_url'),
     url(r'^requests/$', requests_log_page, name='requests_url'),
-    # Examples:
-    # url(r'^$', 'testproject.views.home', name='home'),
-    # url(r'^testproject/', include('testproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
