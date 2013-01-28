b337a853426aa8ad655a28a01c2fc2520114a022
animasola <annima.sola@gmail.com>
2013-01-28 20:11:36 +0200

    ticket:8 fixes according to coments

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=50, verbose_name='Name')
     last_name = models.CharField(max_length=70, verbose_name='Last Name')
-    birth_date = models.DateField(auto_now=False, auto_now_add=False,
-                verbose_name='Date of birth')
+    birth_date = models.DateField(
+        auto_now=False, auto_now_add=False, verbose_name='Date of birth')
     bio = models.TextField(verbose_name='Bio')
     email = models.EmailField(max_length=75, verbose_name='Email')
     jabber = models.CharField(max_length=50, verbose_name='Jabber')
